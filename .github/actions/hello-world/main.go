3c8fd1cfc483bf3d8f868a07d26e460df2e4b1db
kyung-choi-ingka-com <64960383+kyung-choi-ingka-com@users.noreply.github.com>
2020-12-10 12:44:28 +0100

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,6 +1,18 @@
 package main
-import "fmt"
+import (
+   "fmt"
+   "os"
+)
 
 func main() {
-   fmt.Println("Hello world")
+   input1 := os.getEnv("INPUT_GREETING1")
+   input2 := os.getEnv("INPUT_GREETING2")
+   input3 := os.getEnv("INPUT_GREETING3")
+   
+   fmt.Println(input1)
+   fmt.Println(intpu2)
+   
+   if input3 != "" {
+      fmt.Println(input3)
+   }
 }
